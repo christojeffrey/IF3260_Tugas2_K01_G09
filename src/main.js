e4ad1b9c5b6f6f1aa4f73f0a693e2690899fdadf
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-03-15 10:56:24 +0700

    proof of concept, simple 3D

--- src/main.js
+++ src/main.js
@@ -1,138 +1,267 @@
-const positions = [
-  // Front face
-  -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0,
+/*============= Creating a canvas ======================*/
+var canvas = document.getElementById("my_Canvas");
+// gl = canvas.getContext("experimental-webgl");
+let gl = canvas.getContext("experimental-webgl");
 
-  // Back face
-  -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0,
+/*========== Defining and storing the geometry ==========*/
 
-  // Top face
-  -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0,
+function initVerticesBuffer(gl) {
+  // Create a buffer for the square's positions.
+  const verticesBuffer = gl.createBuffer();
 
-  // Bottom face
-  -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,
+  // Select the verticesBuffer as the one to apply buffer
+  // operations to from here out.
+  gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
 
-  // Right face
-  1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0,
-
-  // Left face
-  -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0,
-];
-
-const numComponents = 3;
-
-const faceColors = [
-  [1.0, 1.0, 1.0, 1.0], // Front face: white
-  [1.0, 0.0, 0.0, 1.0], // Back face: red
-  [0.0, 1.0, 0.0, 1.0], // Top face: green
-  [0.0, 0.0, 1.0, 1.0], // Bottom face: blue
-  [1.0, 1.0, 0.0, 1.0], // Right face: yellow
-  [1.0, 0.0, 1.0, 1.0], // Left face: purple
-];
-
-// Convert the array of colors into a table for all the vertices.
-
-var colors = [];
-
-for (var j = 0; j < faceColors.length; ++j) {
-  const c = faceColors[j];
-  // Repeat each color four times for the four vertices of the face
-  colors = colors.concat(c, c, c, c);
-}
-
-function initIndexBuffer(gl) {
-  const indexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-  // This array defines each face as two triangles, using the
-  // indices into the vertex array to specify each triangle's
-  // position.
-
-  const indices = [
-    0,
-    1,
-    2,
-    0,
-    2,
-    3, // front
-    4,
-    5,
-    6,
-    4,
-    6,
-    7, // back
-    8,
-    9,
-    10,
-    8,
-    10,
-    11, // top
-    12,
-    13,
-    14,
-    12,
-    14,
-    15, // bottom
-    16,
-    17,
-    18,
-    16,
-    18,
-    19, // right
-    20,
-    21,
-    22,
-    20,
-    22,
-    23, // left
+  // Now create an array of positions for the square.
+  var vertices = [
+    -1, -1, -1, 1, -1, -1, 1, 1, -1, -1, 1, -1, -1, -1, 1, 1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, -1, -1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, 1, 1, 1, 1, -1, 1, -1, -1, -1, -1, -1, 1, 1, -1, 1, 1, -1, -1, -1, 1, -1, -1, 1, 1, 1,
+    1, 1, 1, 1, -1,
   ];
 
-  // Now send the element array to GL
+  // Now pass the list of positions into WebGL to build the
+  // shape. We do this by creating a Float32Array from the
+  // JavaScript array, then use it to fill the current buffer.
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-
-  return indexBuffer;
+  return verticesBuffer;
 }
 
-const indexBuffer = initIndexBuffer(gl);
+function initColorBuffer(gl) {
+  let colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  var colors = [5, 3, 7, 5, 3, 7, 5, 3, 7, 5, 3, 7, 1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  return colorBuffer;
+}
 
-return {
-  position: positionBuffer,
-  color: colorBuffer,
-  indices: indexBuffer,
+var indices = [0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23];
+function initIndicesBuffer(gl) {
+  let indicesBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+  return indicesBuffer;
+}
+
+// Create and store data into vertex buffer
+// var vertex_buffer = gl.createBuffer();
+// gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+// gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+let vertex_buffer = initVerticesBuffer(gl);
+
+// Create and store data into color buffer
+// var color_buffer = gl.createBuffer();
+// gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+// gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+let color_buffer = initColorBuffer(gl);
+
+// Create and store data into index buffer
+// var index_buffer = gl.createBuffer();
+// gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
+// gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+let index_buffer = initIndicesBuffer(gl);
+/*=================== SHADERS =================== */
+
+var vertCode =
+  "attribute vec3 position;" +
+  "uniform mat4 Pmatrix;" +
+  "uniform mat4 Vmatrix;" +
+  "uniform mat4 Mmatrix;" +
+  "attribute vec3 color;" + //the color of the point
+  "varying vec3 vColor;" +
+  "void main(void) { " + //pre-built function
+  "gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);" +
+  "vColor = color;" +
+  "}";
+
+var fragCode = "precision mediump float;" + "varying vec3 vColor;" + "void main(void) {" + "gl_FragColor = vec4(vColor, 1.);" + "}";
+
+// var vertShader = gl.createShader(gl.VERTEX_SHADER);
+// gl.shaderSource(vertShader, vertCode);
+// gl.compileShader(vertShader);
+let vertShader = loadShader(gl, gl.VERTEX_SHADER, vertCode);
+
+// var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+// gl.shaderSource(fragShader, fragCode);
+// gl.compileShader(fragShader);
+let fragShader = loadShader(gl, gl.FRAGMENT_SHADER, fragCode);
+
+var shaderprogram = gl.createProgram();
+gl.attachShader(shaderprogram, vertShader);
+gl.attachShader(shaderprogram, fragShader);
+gl.linkProgram(shaderprogram);
+
+/*======== Associating attributes to vertex shader =====*/
+var _Pmatrix = gl.getUniformLocation(shaderprogram, "Pmatrix");
+var _Vmatrix = gl.getUniformLocation(shaderprogram, "Vmatrix");
+var _Mmatrix = gl.getUniformLocation(shaderprogram, "Mmatrix");
+
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+var _position = gl.getAttribLocation(shaderprogram, "position");
+gl.vertexAttribPointer(_position, 3, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(_position);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+var _color = gl.getAttribLocation(shaderprogram, "color");
+gl.vertexAttribPointer(_color, 3, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(_color);
+gl.useProgram(shaderprogram);
+
+/*==================== MATRIX ====================== */
+
+function get_projection(angle, a, zMin, zMax) {
+  var ang = Math.tan((angle * 0.5 * Math.PI) / 180); //angle*.5
+  return [0.5 / ang, 0, 0, 0, 0, (0.5 * a) / ang, 0, 0, 0, 0, -(zMax + zMin) / (zMax - zMin), -1, 0, 0, (-2 * zMax * zMin) / (zMax - zMin), 0];
+}
+
+var proj_matrix = get_projection(40, canvas.width / canvas.height, 1, 100);
+var mo_matrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+var view_matrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+
+view_matrix[14] = view_matrix[14] - 6;
+
+/*================= Mouse events ======================*/
+
+var AMORTIZATION = 0.95;
+var drag = false;
+var old_x, old_y;
+var dX = 0,
+  dY = 0;
+
+var mouseDown = function (e) {
+  drag = true;
+  (old_x = e.pageX), (old_y = e.pageY);
+  e.preventDefault();
+  return false;
 };
 
-// Tell WebGL which indices to use to index the vertices
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+var mouseUp = function (e) {
+  drag = false;
+};
 
-{
-  const vertexCount = 36;
-  const type = gl.UNSIGNED_SHORT;
-  const offset = 0;
-  gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+var mouseMove = function (e) {
+  if (!drag) return false;
+  (dX = ((e.pageX - old_x) * 2 * Math.PI) / canvas.width), (dY = ((e.pageY - old_y) * 2 * Math.PI) / canvas.height);
+  THETA += dX;
+  PHI += dY;
+  (old_x = e.pageX), (old_y = e.pageY);
+  e.preventDefault();
+};
+
+canvas.addEventListener("mousedown", mouseDown, false);
+canvas.addEventListener("mouseup", mouseUp, false);
+canvas.addEventListener("mouseout", mouseUp, false);
+canvas.addEventListener("mousemove", mouseMove, false);
+
+/*=========================rotation================*/
+
+function rotateX(m, angle) {
+  var c = Math.cos(angle);
+  var s = Math.sin(angle);
+  var mv1 = m[1],
+    mv5 = m[5],
+    mv9 = m[9];
+
+  m[1] = m[1] * c - m[2] * s;
+  m[5] = m[5] * c - m[6] * s;
+  m[9] = m[9] * c - m[10] * s;
+
+  m[2] = m[2] * c + mv1 * s;
+  m[6] = m[6] * c + mv5 * s;
+  m[10] = m[10] * c + mv9 * s;
 }
 
-let cubeRotation = 0.0;
+function rotateY(m, angle) {
+  var c = Math.cos(angle);
+  var s = Math.sin(angle);
+  var mv0 = m[0],
+    mv4 = m[4],
+    mv8 = m[8];
 
-function drawScene(gl, programInfo, buffers, cubeRotation) {
-  mat4.rotate(
-    modelViewMatrix, // destination matrix
-    modelViewMatrix, // matrix to rotate
-    cubeRotation, // amount to rotate in radians
-    [0, 0, 1]
-  ); // axis to rotate around (Z)
-  mat4.rotate(
-    modelViewMatrix, // destination matrix
-    modelViewMatrix, // matrix to rotate
-    cubeRotation * 0.7, // amount to rotate in radians
-    [0, 1, 0]
-  ); // axis to rotate around (Y)
-  mat4.rotate(
-    modelViewMatrix, // destination matrix
-    modelViewMatrix, // matrix to rotate
-    cubeRotation * 0.3, // amount to rotate in radians
-    [1, 0, 0]
-  ); // axis to rotate around (X)
+  m[0] = c * m[0] + s * m[2];
+  m[4] = c * m[4] + s * m[6];
+  m[8] = c * m[8] + s * m[10];
+
+  m[2] = c * m[2] - s * mv0;
+  m[6] = c * m[6] - s * mv4;
+  m[10] = c * m[10] - s * mv8;
 }
 
-drawScene(gl, programInfo, buffers, cubeRotation);
-cubeRotation += deltaTime;
+/*=================== Drawing =================== */
+
+var THETA = 0,
+  PHI = 0;
+var time_old = 0;
+
+var animate = function (time) {
+  var dt = time - time_old;
+
+  if (!drag) {
+    (dX *= AMORTIZATION), (dY *= AMORTIZATION);
+    (THETA += dX), (PHI += dY);
+  }
+
+  //set model matrix to I4
+
+  (mo_matrix[0] = 1),
+    (mo_matrix[1] = 0),
+    (mo_matrix[2] = 0),
+    (mo_matrix[3] = 0),
+    (mo_matrix[4] = 0),
+    (mo_matrix[5] = 1),
+    (mo_matrix[6] = 0),
+    (mo_matrix[7] = 0),
+    (mo_matrix[8] = 0),
+    (mo_matrix[9] = 0),
+    (mo_matrix[10] = 1),
+    (mo_matrix[11] = 0),
+    (mo_matrix[12] = 0),
+    (mo_matrix[13] = 0),
+    (mo_matrix[14] = 0),
+    (mo_matrix[15] = 1);
+
+  rotateY(mo_matrix, THETA);
+  rotateX(mo_matrix, PHI);
+
+  time_old = time;
+  gl.enable(gl.DEPTH_TEST);
+
+  // gl.depthFunc(gl.LEQUAL);
+
+  gl.clearColor(0.5, 0.5, 0.5, 0.9);
+  gl.clearDepth(1.0);
+  gl.viewport(0.0, 0.0, canvas.width, canvas.height);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  gl.uniformMatrix4fv(_Pmatrix, false, proj_matrix);
+  gl.uniformMatrix4fv(_Vmatrix, false, view_matrix);
+  gl.uniformMatrix4fv(_Mmatrix, false, mo_matrix);
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
+  gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+
+  window.requestAnimationFrame(animate);
+};
+animate(0);
+
+function loadShader(gl, type, source) {
+  const shader = gl.createShader(type);
+
+  // Send the source to the shader object
+
+  gl.shaderSource(shader, source);
+
+  // Compile the shader program
+
+  gl.compileShader(shader);
+
+  // See if it compiled successfully
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(`An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`);
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}

b573a6e35419731a2b07736d91de4764f0167275
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-03-24 18:13:25 +0700

    feat: adding help page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -81,105 +81,48 @@ async function main() {
   // let rotation = [degToRad(0), degToRad(0), degToRad(0)];
   // let translation = [550, 350, 10];
   // let scale = [1, 1, 1];
-//   let config   =   {
-//     // rotation:       [degToRad(115), degToRad(180), degToRad(225)],
-//     rotation:      [degToRad(0), degToRad(0), degToRad(0)],
-//     translation:   [0, 0, 0],
-//     // translation:    [-750, 250, 500],
-//     // translation:    [250, -250, 0],
-//     scale:          [1, 1, 1],
-// }
-//   console.log(config);
-  let object          = {
-    name        : "rubic",
-    positions   : rubic.position,
-    colors      : rubic.colors,
-    normals     : rubic.normals,
-  }
-  let totalVertices   = object.positions.length / 3;
-  let projection      = "orthographic";
-  let rotation        = [degToRad(0), degToRad(0), degToRad(0)];
-  let translation     = [0, 0, 0];
-  let scale           = [1, 1, 1];
-  let camera          = {radius: 0, angles: degToRad(0)}
-  let shading         = true;
-  let animate         = false;
+  //   let config   =   {
+  //     // rotation:       [degToRad(115), degToRad(180), degToRad(225)],
+  //     rotation:      [degToRad(0), degToRad(0), degToRad(0)],
+  //     translation:   [0, 0, 0],
+  //     // translation:    [-750, 250, 500],
+  //     // translation:    [250, -250, 0],
+  //     scale:          [1, 1, 1],
+  // }
+  //   console.log(config);
+  let object = {
+    name: "rubic",
+    positions: rubic.position,
+    colors: rubic.colors,
+    normals: rubic.normals,
+  };
+  // let totalVertices = object.positions.length / 3;
+  // let projection = "orthographic";
+  // let rotation = [degToRad(0), degToRad(0), degToRad(0)];
+  // let translation = [0, 0, 0];
+  // let scale = [1, 1, 1];
+  // let camera = { radius: 0, angles: degToRad(0) };
+  // let shading = true;
+  // let animate = false;
 
-  let defaultConditions = {
-    object, 
-    totalVertices,
-    projection,  
-    rotation, 
-    translation, 
-    scale, 
-    camera,
-    shading, 
-    animate 
-  }
+  // let defaultConditions = {
+  //   object,
+  //   totalVertices,
+  //   projection,
+  //   rotation,
+  //   translation,
+  //   scale,
+  //   camera,
+  //   shading,
+  //   animate,
+  // };
 
   // Setup a ui.
 
-  let state = setupUI(gl)
+  let state = setupUI(gl);
 
-
-
-  
-  
-  // setup export import
-  // document.querySelector("#export").addEventListener("click", () => {
-  //   const data = {
-  //     positions,
-  //     colors,
-  //   };
-  //   const json = JSON.stringify(data);
-  //   const blob = new Blob([json], { type: "application/json" });
-  //   const url = URL.createObjectURL(blob);
-  //   const a = document.createElement("a");
-  //   a.download = "data.json";
-
-  //   a.href = url;
-  //   a.click();
-  //   drawScene(gl, program, buffers, state);
-  // });
-
-  // // on import Button click, read file on input #importFile
-  // document.querySelector("#import").addEventListener("click", () => {
-  //   const file = document.querySelector("#importFile").files[0];
-  //   const reader = new FileReader();
-  //   reader.onload = (e) => {
-  //     const data = JSON.parse(e.target.result);
-  //     positions = data.positions;
-  //     colors = data.colors;
-  //     buffers = initBuffers(gl, positions, colors);
-  //     state.totalVertices = positions.length / 3;
-  //     drawScene(gl, program, buffers, state);
-  //   };
-  //   reader.readAsText(file);
-  // });
-
-  // clear canvas
-  // document.querySelector("#clear").addEventListener("click", () => {
-  //   positions = [];
-  //   colors = [];
-  //   buffers = initBuffers(gl, positions, colors);
-  //   state.totalVertices = 0;
-  //   drawScene(gl, program, buffers, state);
-  // });
-
-  // setup slider
-  // setupSlider("#x", { value: translation[0], slide: updatePosition(0), max: gl.canvas.width });
-  // setupSlider("#y", { value: translation[1], slide: updatePosition(1), max: gl.canvas.height });
-  // setupSlider("#z", { value: translation[2], slide: updatePosition(2), max: gl.canvas.height });
-  // setupSlider("#angleX", { value: radToDeg(rotation[0]), slide: updateRotation(0), max: 360 });
-  // setupSlider("#angleY", { value: radToDeg(rotation[1]), slide: updateRotation(1), max: 360 });
-  // setupSlider("#angleZ", { value: radToDeg(rotation[2]), slide: updateRotation(2), max: 360 });
-  // setupSlider("#scaleX", { value: scale[0], slide: updateScale(0), min: -5, max: 5, step: 0.01, precision: 2 });
-  // setupSlider("#scaleY", { value: scale[1], slide: updateScale(1), min: -5, max: 5, step: 0.01, precision: 2 });
-  // setupSlider("#scaleZ", { value: scale[2], slide: updateScale(2), min: -5, max: 5, step: 0.01, precision: 2 });
-
-  let then = 0;
+  // let then = 0;
   renderScene(gl, program, state);
-  // Draw the scene repeatedly
 }
 
 main();
